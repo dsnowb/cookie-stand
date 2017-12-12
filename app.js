ae90cca2f18d404873a9ab0e1302acff98b07e68
Dave Snowberger <git@fartherout.org>
2017-12-11 19:48:52 -0800

    added a little documentation and cleaned up the .js files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//NOTE: This script depends on stores.js being loaded before it.
+
 'use strict';
 
+//Looks for an element with class 'hourLoc' and prints into it a series of <li>'s consisting of a store location and its respective hours
 var hourLoc = document.getElementsByClassName('hourLoc')[0];
 for (var i = 0; i < arrStores.length; i++)
   hourLoc.innerHTML += '<li>' + arrStores[i].storeLocale + ': ' + arrStores[i].hourToStd(arrStores[i].hourOpen) + '-' + arrStores[i].hourToStd(arrStores[i].hourClose) + '</li>';
