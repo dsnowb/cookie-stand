52f5d9e641c28d46fd49eb1f5fe5e8dbfeb5549e
Dave Snowberger <git@fartherout.org>
2017-12-12 13:31:32 -0800

    did a crap load of stuff, really. pulled a couple of functions out of the objects to be global helper functions instead, started changing the rendering of sales data from UL to table

--- sales.js
+++ sales.js
@@ -5,5 +5,5 @@
 //Generates a random number of cookies purchased per hour of store operation, then prints that information for each location
 for (var i = 0; i < arrStores.length; i++) {
   arrStores[i].genEstCookiesPerHour();
-  arrStores[i].printSales();
+  arrStores[i].renderSalesRow();
 }
--- stores.js
+++ stores.js
@@ -1,18 +1,40 @@
 'use strict';
 
-//specific store data
+//specific store data array
+//array data is of the form [store location, opening hour in military time, closing hour in military time, minimum
+//number of customers, maximum number of customers, average cookies per customer]
 //*******************************************************************************************************************
 var arrStoreData = [  ['1st and Pike', 6, 20, 23, 65, 6.3],
-                  ['SeaTac Airport', 6, 20, 3, 24, 1.2],
-                  ['Seattle Center', 6, 20, 11, 38, 3.7],
-                  ['Capitol Hill', 6, 20, 20, 38, 2.3],
-                  ['Alki', 6, 20, 2, 16, 4.6]
-               ];
+                      ['SeaTac Airport', 5, 20, 3, 24, 1.2],
+                      ['Seattle Center', 6, 20, 11, 38, 3.7],
+                      ['Capitol Hill', 6, 20, 20, 38, 2.3],
+                      ['Alki', 6, 20, 2, 16, 4.6] ];
 
+//The earliest and latest store hours among all locations
+var earlyHour = 5;
+var lateHour = 20;
+
+//Global helper functions
+//*******************************************************************************************************************
+//Converts from military time to standard time
+var hourToStd = function(hour) {
+    if (hour < 12) hour += 'am';
+    else if (hour === 12) hour += 'pm';
+    else {
+      hour -= 12;
+      hour += 'pm';
+    }
+    return hour;
+}
+
+//Generates a random integer between min and max, inclusive
+var random = function(min,max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
 
 //store object properties
 //*******************************************************************************************************************
-function store(locale,hrOpen,hrClose,minCust,maxCust,cookiePerHr) {
+function Store(locale,hrOpen,hrClose,minCust,maxCust,cookiePerHr) {
 
   this.storeLocale = locale;
   this.hourOpen = hrOpen;
@@ -26,49 +48,37 @@ function store(locale,hrOpen,hrClose,minCust,maxCust,cookiePerHr) {
 
 //store object prototype methods
 //*******************************************************************************************************************
-//Generates a random value between minCustomers and maxCustomers, inclusive
-store.prototype.genCustPerHour = function() {
-    return Math.round(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
-}
-
-//Converts from military time to standard time
-store.prototype.hourToStd = function(hour) {
-    if (hour < 12) hour += 'am';
-    else if (hour === 12) hour += 'pm';
-    else {
-      hour -= 12;
-      hour += 'pm';
-    }
-    return hour;
-}
-
-//Uses genCustPerHour() to generate a random number of cookies sold for each hour of operation, then returns an array
-//containing two elements. First element: a given hour of operation. Second element: the randomly generated number of
-//cookies purchases for that hour
-store.prototype.genEstCookiesPerHour = function() {
+//Uses genCustPerHour() to generate a random number of cookies sold for each hour of operation, fills the estCookiesPerHour
+//array with two elements. First: a given hour of operation. Second: the randomly generated number of cookies purchases for
+//that hour. Finally, it provides a total number of cookies purchased for the day.
+Store.prototype.genEstCookiesPerHour = function() {
+    for (var i = 0; i < lateHour-earlyHour; i++)
+      this.estCookiesPerHour.push([earlyHour+i]);
     for (var i = 0; i < this.hourClose - this.hourOpen; i++) {
-      this.estCookiesPerHour.push([this.hourToStd(this.hourOpen + i), Math.round(this.genCustPerHour() * this.cookiesPerCust)]);
-      this.estTotalCookies += this.estCookiesPerHour[i][1];
+      this.estCookiesPerHour[this.hourOpen-earlyHour+i].push(Math.round(random(this.minCustomers,this.maxCustomers) * this.cookiesPerCust));
+      this.estTotalCookies += this.estCookiesPerHour[this.hourOpen-earlyHour+i][1];
     }
-
-    return this.estCookiesPerHour;
 }
 
 //Prints an unordered list to the first element tagged 'main' in the DOM. The UL is given a title of storeLocale,
 //followed by a formatted version of the current values in the estCookiesPerHour array
-store.prototype.printSales = function() {
-    var ul = document.createElement('ul');
-    var list = '';
-    for (var i = 0; i < this.estCookiesPerHour.length; i++)
-      list += '<li>' + this.estCookiesPerHour[i][0] + ': ' + this.estCookiesPerHour[i][1] + ' cookies</li>\n';
-    ul.innerHTML = '<p class="ulp">' + this.storeLocale + '</p>\n' + list + '<li>Total: ' + this.estTotalCookies + ' cookies</li>';
-    document.getElementsByTagName('main')[0].appendChild(ul);
+Store.prototype.renderSalesRow = function() {
+    var trEl = document.createElement('tr');
+    for (var i = 0; i < lateHour-earlyHour; i++) {
+      var tdEl = document.createElement('td');
+      tdEl.textContent = this.estCookiesPerHour[i][1];
+      trEl.appendChild(tdEl);
+    }
+
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.estTotalCookies;
+    trEl.appendChild(tdEl);
+
+    document.getElementById('sales_table').appendChild(trEl);
 }
 
-//generate store objects and put them into array
+//generate store objects and put them into array for use by associated scripts
 //*******************************************************************************************************************
 var arrStores = [];
 for (var i = 0; i < arrStoreData.length; i++)
-  arrStores.push(new store(arrStoreData[i][0], arrStoreData[i][1], arrStoreData[i][2], arrStoreData[i][3], arrStoreData[i][4], arrStoreData[i][5]));
-
-console.log(arrStores);
+  arrStores.push(new Store(arrStoreData[i][0], arrStoreData[i][1], arrStoreData[i][2], arrStoreData[i][3], arrStoreData[i][4], arrStoreData[i][5]));
